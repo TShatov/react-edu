1ef3203095b90f941f74f8744d6f0f9411503e75
TShatov <tshatov@gmail.com>
2023-01-10 22:58:30 +0300

    feat: new modules

---
+++ index.js
@@ -0,0 +1,13 @@
+import { MathOperations } from "./src/mathoperations";
+
+const result = MathOperations.multiply(2,3);
+console.log(result);
+
+const result2 = MathOperations.sum(2,5);
+console.log(result2);
+
+const result3 = MathOperations.divide(5,2);
+console.log(result3);
+
+const result4 = MathOperations.subtract(2,5);
+console.log(result4);
